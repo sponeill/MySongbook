677e009522248030b594da0167665fd7b1221070
Shane O'Neill <oneill.shanepatrick@gmail.com>
2018-03-12 09:49:35 -0400

    Add project files.

---
+++ MySongbook/Global.asax.cs
@@ -0,0 +1,38 @@
+﻿using Ninject;
+using Ninject.Web.Common.WebHost;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+namespace MySongbook
+{
+	public class MvcApplication : NinjectHttpApplication
+	{
+		protected override void OnApplicationStarted()
+		{
+			base.OnApplicationStarted();
+
+			AreaRegistration.RegisterAllAreas();
+			FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+			RouteConfig.RegisterRoutes(RouteTable.Routes);
+			BundleConfig.RegisterBundles(BundleTable.Bundles);
+		}
+
+		protected override IKernel CreateKernel()
+		{
+			var kernel = new StandardKernel();
+
+			string connectionString = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+
+			// Configure Bindings
+			// kernel.Bind<interface>().To<class>();
+
+			return kernel;
+		}
+	}
+}
---
+++ MySongbook/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MySongbook.Startup))]
+namespace MySongbook
+{
+	public partial class Startup
+	{
+		public void Configuration(IAppBuilder app)
+		{
+			ConfigureAuth(app);
+		}
+	}
+}
