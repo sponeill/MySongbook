407975a32b1e32e1a91b930dbfc17e7e2e70bb6a
Shane O'Neill <oneill.shanepatrick@gmail.com>
2018-03-13 14:23:42 -0400

    CSS

---
+++ MySongbook/Models/Quotes.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MySongbook.Models
+{
+	public class Quotes
+	{
+		List<string> QuoteArray = new List<string> { "'Music is a moral law. It gives soul to the universe, wings to the mind, flight to the imagination ...' -Plato", "'Music doesn’t lie. If there is something to be changed in this world, then it can only happen through music.' – Jimi Hendrix", "'Always be a first-rate version of yourself, instead of a second-rate version of somebody else.' – Judy Garland" , "'Music expresses that which cannot be said and on which it is impossible to be silent.' – Victor Hugo" };
+
+	}
+}
